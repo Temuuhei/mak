31e07519d3cc32e90bb1c45da0ed27aa285a1bbc
Temuuhei <temuujintsogt@gmail.com>
2017-09-08 16:49:23 +0900

    [ADD] ДХМТГ ажлын бүртгэлийн модуль

---
+++ mak_audit/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Mongolyn Alt LLC, Enterprise Management Solution
+#    Copyright (C) 2013-2015 Asterisk Technologies LLC (<http://www.erp.mn/, http://asterisk-tech.mn/&gt;). All Rights Reserved
+#
+#    Email : temuujintsogt@gmail.com
+#    Phone : 976 - 99741074
+#
+##############################################################################
+{
+    "name" : "MAK Дотоод хяналт МТХ",
+    "version" : "1.0",
+    "author" : "Temuujin",
+    "description": """
+      MAK Audit Module
+""",
+    "website" : False,
+    "category" : "MAK",
+    "depends" : [],
+    "init": [],
+    "update_xml" : [
+        'views/mak_audit_views.xml',
+        'views/mak_audit_sequence.xml',],
+    "demo_xml": [],
+    'icon': '/logo/mak.png',
+    "active": False,
+    "installable": True,
+    # "auto_install":True,
+}
---
+++ mak_audit/model/mak_audit.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Mongolyn Alt LLC, Enterprise Management Solution
+# Copyright (C) 2013-2015 Asterisk Technologies LLC (<http://www.erp.mn/, http://asterisk-tech.mn/&gt;). All Rights Reserved #
+# Email : temuujintsogt@gmail.com
+# Phone : 976 + 99741074
+#
+##############################################################################
+import time
+import math
+import openerp.pooler
+import openerp.tools
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+import openerp.netsvc
+import sys
+import logging
+from openerp.addons.crm import crm
+from datetime import timedelta
+from datetime import datetime
+from datetime import date
+from dateutil import parser
+from openerp import SUPERUSER_ID
+from openerp.http import request
+from lxml import etree
+_logger = logging.getLogger('openerp')
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+DATE_FORMAT = "%Y-%m-%d"
+
+
+class mak_audit(osv.osv):
+    _name = 'mak.audit'
+    _description = "MAK Audit department"
+
+    # Өдөрөөр салгах
+    def _set_date(self, cr, uid, ids, name, args, context=None):
+        res = {}
+        obj = self.browse(cr, uid, ids)[0]
+        date_object = datetime.strptime(obj.received_date, '%Y-%m-%d %H:%M:%S')
+        res[obj.id] = {
+            'year': date_object.year,
+            'month': date_object.month,
+            'day': date_object.day
+        }
+        return res
+
+    _columns = {
+        'user_id': fields.many2one('res.users', 'Employee', required=True, readonly=True),
+        'sequence_id': fields.char('Audit Sequence', size=32, required=True),
+        'num_received_document': fields.char('Number of Received Documents',required = True),
+        'received_date': fields.datetime('Received date',required = True),
+        'department_id': fields.many2one('hr.department','Sector', domain = [('type', '=', 'sector')]),
+        'type_doc': fields.selection([('budget','Budget'),('description','Description')], 'Document Type'),
+        'doc_name': fields.text('Name',size=160,required = True),
+        'partner_id': fields.many2one('res.partner','Partner',required = True),
+        'location': fields.char('Location'),
+        'received_value': fields.float('Receive Value'),
+        'check_budget': fields.float('Check Budget'),
+        'difference': fields.float('Difference'),
+        'num_delivery_document': fields.char('Number of Delivery Documents',required = True),
+        'description': fields.text('Description',size=160,required = True),
+        'year': fields.function(_set_date, type='integer', string='Year', multi='dates', readonly=True, store=True),
+        'month': fields.function(_set_date, type='integer', string='Month', multi='dates', readonly=True, store=True),
+        'day': fields.function(_set_date, type='integer', string='Day', multi='dates', readonly=True, store=True),
+    }
+
+    _defaults = {
+        'user_id': lambda s, cr, uid, c: uid,
+        'received_date': fields.datetime.now,
+        'sequence_id':'/',
+    }
+
+    def onchange_diff(self, cr, uid, ids, received_value, check_budget, difference, context=None):
+        res = {}
+        diff_value = ''
+        if received_value and check_budget:
+            diff_value = received_value - check_budget
+            res.update({'difference': diff_value})
+
+        return {'value': res}
+
+    def create (self, cr, uid, vals, context=None):
+        vals['sequence_id'] = self.pool.get('ir.sequence').get(cr, uid, 'mak.audit')
+        audit = super(mak_audit, self).create(cr, uid, vals, context=context)
+        return audit
