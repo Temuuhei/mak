ab9de657a56c78ca37cc10fea5046522b9763aca
Temuuhei <temuujintsogt@gmail.com>
2021-06-03 13:46:43 +0800

    [IMP] ADD some modules

--- mak_security/model/mak_security.py
+++ mak_security/model/mak_security.py
@@ -6,18 +6,23 @@
 #
 #    Email : munkhbat.k@mak.mn
 #    Phone : 99008457
-#
+#    Date  : 2021/05/19
 ##############################################################################
 
 from openerp.osv import osv, fields
 from datetime import datetime
-from datetime import date
 
-class mak_security(osv.Model):
+class hr_department(osv.osv):
+    _inherit = 'hr.department'
+
+    _columns = {
+        'is_sec' : fields.boolean('Is Security', default = False)
+    }
+class mak_security(osv.osv):
     _name = 'mak.security'
     _descriptions = "MAK security report"
     _inherit = 'mail.thread'
-    _order = "date desc"
+    _order = "start_datetime desc"
 
     STATE_SELECTION = [
         ('draft', 'Draft'),
@@ -27,7 +32,7 @@ class mak_security(osv.Model):
     def _set_date(self, cr, uid, ids, name, args, context=None):
         res = {}
         obj = self.browse(cr, uid, ids)[0]
-        date_object = datetime.strptime(obj.date, '%Y-%m-%d')
+        date_object = datetime.strptime(obj.start_datetime, '%Y-%m-%d %H:%M:%S')
         res[obj.id] = {
             'year': date_object.year,
             'month': date_object.month,
@@ -36,41 +41,36 @@ class mak_security(osv.Model):
         return res
 
     _columns = {
-        'department_id': fields.many2one('hr.department', string='Sector'),
+        'department_id': fields.many2one('hr.department', string='Department',readonly=1),
+        'point': fields.char('Point', states={'sent':[('readonly',True)]}),
+        'employee_id': fields.many2one('hr.employee', string='Assigned To', states={'sent':[('readonly',True)]}),
+        'received_employee_id': fields.many2one('hr.employee',  string='Received', track_visiblity='onchange', states={'sent':[('readonly',True)]}),
         'year': fields.function(_set_date, type='integer', string='Year', multi='dates', readonly=True, store=True),
         'month': fields.function(_set_date, type='integer', string='Month', multi='dates', readonly=True, store=True),
         'day': fields.function(_set_date, type='integer', string='Day', multi='dates', readonly=True, store=True),
-        'date': fields.date('Date', default = date.today(), states={'sent': [('readonly', True)]}),
-        'breach': fields.selection([('yes', 'Yes'),('no', 'No')], 'Breach', track_visibility='onchange', states={'sent': [('readonly', True)]}),
-        'description': fields.text('Description', size=400, states={'sent': [('readonly', True)]}),
+        'start_datetime': fields.datetime('Start date', states={'sent':[('readonly',True)]}),
+        'end_datetime': fields.datetime('End date', states={'sent':[('readonly',True)]}),
+        'problem': fields.selection([('yes', 'Notnormal'), ('no', 'Normal')], 'Problem', track_visibility='onchange', states={'sent':[('readonly',True)]}),
+        'note_motion': fields.text('Note motion', size=500, states={'sent':[('readonly',True)]}),
+        'note_electric': fields.text('Note electric', size=200, states={'sent':[('readonly',True)]}),
+        'note_car': fields.text('Note car', size=200, states={'sent':[('readonly',True)]}),
+        'note_work': fields.text('Note work', size=200, states={'sent':[('readonly',True)]}),
+        'note_check': fields.text('Note work', size=300, states={'sent':[('readonly',True)]}),
+        'note_gate': fields.text('Note gate', size=300, states={'sent':[('readonly',True)]}),
+        'note_time': fields.text('Note time', size=200, states={'sent':[('readonly',True)]}),
+        'note_security': fields.text('Note security', size=300, states={'sent':[('readonly',True)]}),
+        'task': fields.text('Task', size=300),
         'state': fields.selection(STATE_SELECTION, 'State', track_visibility='onchange')
     }
 
-    def get_sector(self, cr, uid, dep_id):
-        dep_obj = self.pool.get('hr.department')
-        department = dep_obj.browse(cr, uid, [dep_id])[0]
-        if department.type == 'sector' or department.type == 'project':
-            return department.id
-        elif department['parent_id'].type == 'sector' or department.type == 'project':
-            return department['parent_id'].id
-        elif not department['parent_id']:
-            return department.id
-        else:
-            return self.get_sector(cr, uid, department['parent_id'].id)
-
     def _department_get(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        ids = self.pool.get('hr.employee').search(cr, uid, [('user_id', '=', uid)], context=context)
-        if ids:
-            dep_id = self.pool.get('hr.employee').browse(cr, uid, ids[0])['department_id'].id
-            dep_id2 = self.get_sector(cr, uid, dep_id)
-            return dep_id2
-        return False
+        dep_id = self.pool.get('res.users').browse(cr, uid, uid)['department_id'].id
+        return dep_id
 
     _defaults = {
         'state': 'draft',
-        'department_id': _department_get,
+        'start_datetime': datetime.now(),
+        'department_id': _department_get
     }
 
     def action_sent(self, cr, uid, ids, context=None):
